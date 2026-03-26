1ee47913e24ce89190f1eef13bd5b1c7bd444d0e
BaraCode1 <bdiouf1986@gmail.com>
2026-03-26 11:28:34 -0400

    essage

--- connexion.php
+++ connexion.php
@@ -1,16 +1,14 @@
 <?php
 
-$hn='localhost';
-$un='root';
-$db='mescoursenligne';
-$pw='';
+$hn = 'localhost';
+$un = 'root';
+$db = 'mescoursenligne';
+$pw = '';
 
-$conn= mysqli_connect($hn,$un,$pw,$db);
-if(!$conn)
-{
-die('Could not Connect My Sql:' .mysql_error());
+$conn = mysqli_connect($hn, $un, $pw, $db);
 
+if (!$conn) {
+    die('Could not Connect MySql: ' . mysqli_connect_error());
 }
 
 ?>
-
